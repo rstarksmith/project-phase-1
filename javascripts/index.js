1f6c34e8cad25de2a9f7a8dc4f0b595ec89fda5b
Rachel <rstark.smith@gmail.com>
2022-05-06 20:04:57 -0500

    navbar link js

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,23 +2,38 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-// listSeriesToDom()
+    displayBooks()
 
 })
 
 
 
 // Node Getters
+//nav bar
+const throneOfGlass = document.getElementById('throne')
+const acotar = document.getElementById('acotar')
+const crescentCity = document.getElementById('crescent')
+const fullCollection = document.getElementById('full-collect')
+const headerLinks = document.querySelectorAll('.header-links')
+
+
+//Event Listeners
+
+function displayBooks(){
+headerLinks.forEach(addEventListener('click', listSeriesToDom))
+}
 
 
 //Event Handlers
 
 function listSeriesToDom () {
+    alert('i was clicked')
 
 }
 
 
-//Event Listeners
+
+
 
 
 
