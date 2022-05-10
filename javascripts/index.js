3076508a4e2015a71ec6f1f0b38f3a624e326e1b
Rachel <rstark.smith@gmail.com>
2022-05-10 13:39:07 -0500

    renderBooksToDom Refactoring

--- javascripts/index.js
+++ javascripts/index.js
@@ -13,7 +13,7 @@ const throneOfGlass = document.getElementById('throne')
 const acotar = document.getElementById('acotar')
 const crescentCity = document.getElementById('crescent')
 const fullCollection = document.getElementById('full-collect')
-const headerLinks = document.querySelectorAll('.header-links')
+const ulHeaderLinks = document.getElementById('nav-mobile')
 
 //** Card */
 const cardContainer = document.getElementById('card-container')
@@ -40,23 +40,9 @@ const resetCardContainer = () => {
 
 
 // Event Listeners
-// const throneLinkEvent = () => {
-//     throneOfGlass.addEventListener('click', )
-// }
-// const acotarLinkEvent = () => {
-//     acotar.addEventListener('click', loadAcotarToDom)
-// }
-// const crescentLinkEvent = () => {
-//     crescentCity.addEventListener('click', loadCrescentToDom)
-// }
-// const fullLinkEvent = () => {
-//     fullCollection.addEventListener('click', loadFullToDom)
-// }
 
 
 // Event Handlers
-
-
 
 
 function getBooks() {
@@ -67,51 +53,24 @@ function getBooks() {
         const acotarArray = resp.filter(book => book.series === 'ACOTAR')
         const crescentArray = resp.filter(book => book.series === 'Crescent City')
         const fullArray = resp
-        throneOfGlass.addEventListener('click', (e) => {
-            e.preventDefault()
-            resetCardContainer()
-            throneArray.forEach(book => createBookCard(book))
-        })
-        acotar.addEventListener('click', (e) => {
-            e.preventDefault()
-            resetCardContainer()
-            acotarArray.forEach(book => createBookCard(book))
-        })
-        crescentCity.addEventListener('click', (e) => {
-            e.preventDefault()
-            resetCardContainer()
-            crescentArray.forEach(book => createBookCard(book))
-        })
-        fullCollection.addEventListener('click', (e) => {
-            e.preventDefault()
-            resetCardContainer()
-            fullArray.forEach(book => createBookCard(book))
-        })
+        function renderBooksToDom() {
+            ulHeaderLinks.addEventListener('click', (e) => {
+                e.preventDefault()
+                resetCardContainer()
+                if(e.target === throneOfGlass) {
+                    throneArray.forEach(book => createBookCard(book))
+                } else if (e.target === acotar) {
+                    acotarArray.forEach(book => createBookCard(book))
+                } else if(e.target === crescentCity) {
+                    crescentArray.forEach(book => createBookCard(book))
+                } else if(e.target === fullCollection) {
+                    fullArray.forEach(book => createBookCard(book))
+                }
+            })
+        }   
+        renderBooksToDom()
     })
 }
-
-// function renderBooksToDom(book, array) {
-//     e.preventDefault()
-//     resetCardContainer()
-//     array.forEach(book => createBookCard(book))
-// }
-
-// function renderBooksToDom() {
-//     headerLinks.forEach(addEventListener('click', (e) => {
-//         e.preventDefault()
-//         resetCardContainer()
-//         if(e.target === throneOfGlass) {
-//             throneArray.forEach(book => createBookCard(book))
-//         } else if (e.target === acotar) {
-//             acotarArray.forEach(book => createBookCard(book))
-//         } else if(e.target === crescentCity) {
-//             crescentArray.forEach(book => createBookCard(book))
-//         } else if(e.target === fullCollection) {
-//             fullArray.forEach(book => createBookCard(book))
-//         }
-//     }
-// }
-
 
 
 function createBookCard (book) {
@@ -125,7 +84,7 @@ function createBookCard (book) {
     const pBookOrder = document.createElement('p')
 
     divRow.className = 'row'
-    divCol.className = 'col s2 m4 l6'
+    divCol.className = 'col s1 m2 l4'
     divCard.className = 'card'
     divImage.className = 'book-image'
     divContent.className = 'card-content'
@@ -163,30 +122,25 @@ function createBookCard (book) {
 
 
 
+// //Refactored
 
-
-
-
-
-
-// //targets clicks on links *keep out unless needed
-
-// displayBooksToDom()
-
-// function displayBooksToDom(){
-// headerLinks.forEach(addEventListener('click', listBySeries))
-// }
-
-// function listBySeries(e) {
-//     e.preventDefault()
-//     if(e.target === throneOfGlass) {
-//         alert('click throne')
-//     } else if (e.target === acotar) {
-//         alert('hi hi hi')
-//     } else if(e.target === crescentCity) {
-//         alert('city')
-//     } else if(e.target === fullCollection) {
-//         alert('full')
-//     }
-// }
-
+ // throneOfGlass.addEventListener('click', (e) => {
+        //     e.preventDefault()
+        //     resetCardContainer()
+        //     throneArray.forEach(book => createBookCard(book))
+        // })
+        // acotar.addEventListener('click', (e) => {
+        //     e.preventDefault()
+        //     resetCardContainer()
+        //     acotarArray.forEach(book => createBookCard(book))
+        // })
+        // crescentCity.addEventListener('click', (e) => {
+        //     e.preventDefault()
+        //     resetCardContainer()
+        //     crescentArray.forEach(book => createBookCard(book))
+        // })
+        // fullCollection.addEventListener('click', (e) => {
+        //     e.preventDefault()
+        //     resetCardContainer()
+        //     fullArray.forEach(book => createBookCard(book))
+        // })
